12c0968478fa784a20c64682be3e764805267e3f
tsv2013 <tsv2013@noreply.github.com>
2023-12-07 15:50:35 +0300

    Added library example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import Example from "./components/Example.vue";
+import Library from "./components/Library.vue";
 import RouterPage from "./components/test/RoutePage.vue";
-const isTest = !!(window as any)["%hammerhead%"];
 </script>
 <template>
-  <Example v-if="!isTest"></Example>
-  <RouterPage v-if="isTest"></RouterPage>
+  <RouterPage></RouterPage>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,11 +8,15 @@ import ThemeTab from "../components/test/ThemeTab.vue";
 import ByPage from "../components/test/ByPage.vue";
 import DropdownCollapse from "../components/test/DropdownCollapse.vue";
 import CustomWidget from "../components/test/CustomWidget.vue";
+import Example from "../components/Example.vue"
+import Library from "../components/Library.vue"
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: "/", component: Example },
+    { path: "/library", component: Library },
     { path: "/testCafe/testcafe", component: Default },
     { path: "/testCafe/preview-theme-switcher", component: ThemeSwitcher },
     { path: "/testCafe/theme-for-preview-option", component: ThemePreview },
